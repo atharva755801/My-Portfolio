2660b9e3607f0b3fb4fdbf14a4c54cc603a15326
atharva755801 <atharvasingh9981@gmail.com>
2023-12-09 00:22:02 +0530

    profilepic5

--- src/components/canvas/ProfilePic.jsx
+++ src/components/canvas/ProfilePic.jsx
@@ -1,25 +1,25 @@
 import React ,{ useState, useEffect }from 'react'
 
-const profilePic = () => {
-  // const [borderRadiusIndex, setBorderRadiusIndex] = useState(0);
+const ProfilePic = () => {
+  const [borderRadiusIndex, setBorderRadiusIndex] = useState(0);
 
-  // useEffect(() => {
-  //   const profilePic = document.getElementById('profilePic');
+  useEffect(() => {
+    const profilePic = document.getElementById('profilePic');
 
-  //   const animateProfile = () => {
-  //     setBorderRadiusIndex((prevIndex) => (prevIndex + 1) % 3);
-  //   };
+    const animateProfile = () => {
+      setBorderRadiusIndex((prevIndex) => (prevIndex + 1) % 3);
+    };
 
-  //   const intervalId = setInterval(animateProfile, 4000); // Adjust the interval as needed
+    const intervalId = setInterval(animateProfile, 4000); // Adjust the interval as needed
 
-  //   return () => clearInterval(intervalId); // Cleanup on component unmount
-  // }, []);
+    return () => clearInterval(intervalId); // Cleanup on component unmount
+  }, []);
 
-  // const borderRadiusValues = [
-  //   '60% 40% 30% 70% 60% 30% 70% 40%',
-  //   '30% 60% 70% 40% 50% 60% 30% 60%',
-  //   '60% 40% 30% 70% 60% 30% 70% 40%',
-  // ];
+  const borderRadiusValues = [
+    '60% 40% 30% 70% 60% 30% 70% 40%',
+    '30% 60% 70% 40% 50% 60% 30% 60%',
+    '60% 40% 30% 70% 60% 30% 70% 40%',
+  ];
 
   return (
     <div className="profile_pic" id="profilePic" >
@@ -27,4 +27,4 @@ const profilePic = () => {
   );
 }
 
-export default profilePic
+export default ProfilePic
